290fad8e70f4bd631e2501ff67aef8d3248f37e3
François Roland <francois.roland@outlook.be>
2021-11-15 13:27:30 +0000

    Test

--- create_tables.js
+++ create_tables.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('monprojetdemo:create_database');
 const connection = require('./db_connect');
 
+
 connection.query(
     'create table students (id int auto_increment primary key, firstName varchar(100), lastName varchar(100))',
     function(err, results, fields) {
