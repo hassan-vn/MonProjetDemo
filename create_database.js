83ee4a049f8187284723bfd8b19dd0fb43d7d8e7
Francois ROLAND <froland@froland.be>
2021-11-27 14:56:44 +0000

    Use environment variables for DB connection.

--- create_database.js
+++ create_database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.DB_HOSTNAME || 'localhost',
+  user: process.env.DB_USERNAME || 'root',
   database: 'mysql'
 });
 connection.query(
--- models/students.js
+++ models/students.js
@@ -1,8 +1,14 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
-const sequelize = new Sequelize('studentdb', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+const db_hostname = process.env.DB_HOSTNAME || 'localhost';
+const db_dbname = process.env.DB_DBNAME || 'studentdb';
+const db_username = process.env.DB_USERNAME || 'root';
+const db_password = process.env.DB_PASSWORD || '';
+const db_dialect = process.env.DB_DIALECT || 'mysql';
+
+const sequelize = new Sequelize(db_dbname, db_username, db_password, {
+    host: db_hostname,
+    dialect: db_dialect
 });
 
 class Student extends Model {}
